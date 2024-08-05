c924f5e04287062e4286be20084849fd3fbb75de
MuhammadRamadan97 <mohammed.ramadan.glal@gmail.com>
2024-08-05 15:10:21 +0300

    another commit

--- app/components/chat/ChatContainer.js
+++ app/components/chat/ChatContainer.js
@@ -8,6 +8,9 @@ import { UserContext } from '@/app/context';
 
 let socket;
 
+if (typeof window !== "undefined") {
+    socket = io('https://chatapp-9974.onrender.com/');
+}
 export default function ChatContainer() {
     const [messages, setMessages] = useState([]);
     const [loading, setLoading] = useState(true);
